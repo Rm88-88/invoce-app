cb1ddf50bba4a25d7cca98d490517a57e64a2218
Rm88-88 <rwbn83137@gmail.com>
2024-12-03 08:39:16 +0200

    Create app.module.ts
    Add app module

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+   ```typescript
+   import { NgModule } from '@angular/core';
+   import { BrowserModule } from '@angular/platform-browser';
+   import { FormsModule } from '@angular/forms';
+   import { HttpClientModule } from '@angular/common/http';
+   import { InvoiceFormComponent } from './invoice-form/invoice-form.component';
+
+   @NgModule({
+     declarations: [
+       InvoiceFormComponent,
+     ],
+     imports: [
+       BrowserModule,
+       FormsModule,
+       HttpClientModule,
+     ],
+     providers: [],
+     bootstrap: [InvoiceFormComponent]
+   })
+   export class AppModule { }
+   ```
